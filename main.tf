a64fa5a661acdc89585e7005bd7a2f929c0bfd78
Piyush Sharma <piyush.pradhan627@gmail.com>
2022-05-23 23:58:58 +0530

    comment added

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# To create a s3 bucket-testing
+# To create a s3 bucket-testing1
 #tfsec:ignore:aws-s3-block-public-acls tfsec:ignore:aws-s3-block-public-policy tfsec:ignore:aws-s3-enable-bucket-encryption tfsec:ignore:aws-s3-ignore-public-acls tfsec:ignore:aws-s3-no-public-buckets tfsec:ignore:aws-s3-encryption-customer-key tfsec:ignore:aws-s3-enable-bucket-logging tfsec:ignore:aws-s3-enable-versioning tfsec:ignore:aws-s3-specify-public-access-block
 resource "aws_s3_bucket" "CREATE_S3_BUCKET" {
   bucket = "my-bignet-jenkins3-bucket"
